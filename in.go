42ff9ef578f25783735400ad689a62456411ca04
Julien Ammous <schmurfy@gmail.com>
2021-09-16 16:45:58 +0200

    set integration_tool default as checkout

--- in.go
+++ in.go
@@ -92,11 +92,11 @@ func Get(request GetRequest, github Github, git Git, outputDir string) (*GetResp
 		if err := git.Rebase(pull.BaseRefName, pull.Tip.OID, request.Params.Submodules); err != nil {
 			return nil, err
 		}
-	case "merge", "":
+	case "merge":
 		if err := git.Merge(pull.Tip.OID, request.Params.Submodules); err != nil {
 			return nil, err
 		}
-	case "checkout":
+	case "checkout", "":
 		if err := git.Checkout(pull.HeadRefName, pull.Tip.OID, request.Params.Submodules); err != nil {
 			return nil, err
 		}
